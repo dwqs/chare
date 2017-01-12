b94e71c40dd9f754fcce499e5bc5ea1153e6e984
dwqs <461147874@qq.com>
2017-01-12 11:29:43 +0800

    get github user info

---
+++ src/config.js
@@ -0,0 +1,24 @@
+/**
+ * Created by pomy on 12/01/2017.
+ * read user's github config
+ */
+
+'use strict';
+
+let exec = require('child_process').execSync;
+
+module.exports = function () {
+    let userName, userEmail;
+
+    try {
+        userName = exec('git config --get user.name');
+        userEmail = exec('git config --get user.email');
+    } catch (e) {
+
+    }
+
+    userName = userName && JSON.stringify(userName.toString().trim()).slice(1, -1);
+    userEmail = userEmail && (' <' + userEmail.toString().trim() + '>');
+
+    return (userName || '') + (userEmail || '');
+};
