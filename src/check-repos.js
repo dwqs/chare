ad51aa607cbeed72ae0b68d7fb11be29efe89112
dwqs <461147874@qq.com>
2017-01-17 15:44:53 +0800

    fix

--- src/check-repos.js
+++ src/check-repos.js
@@ -54,8 +54,8 @@ module.exports = function (repo,done){
     }).catch((err) => {
         let res = err.response;
         if(err){
-            spinner.text = chalk.white(`chare cli:checking template ${repo} failed from github.com, error message as follows:`);
-            spinner.fail();
+            oraer.text = chalk.white(`chare cli:checking template ${repo} failed from github.com, error message as follows:`);
+            oraer.fail();
 
             log.tips();
             log.tips(chalk.red(`     ${res.statusText}: ${res.headers.status}`));
