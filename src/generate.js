a6f426944c29b83a4c281f39ee2f47660955cfaa
dwqs <461147874@qq.com>
2017-01-18 17:19:54 +0800

    handlebars

--- src/generate.js
+++ src/generate.js
@@ -7,6 +7,7 @@
 let Metalsmith = require('metalsmith');
 let ora = require('ora');
 let async = require('async');
+let Handlebars = require('handlebars');
 let render = require('consolidate').handlebars.render;
 let path = require('path');
 let chalk = require('chalk');
