93ca344698cfb1f52f9ae096679c4e82de0ed20e
dwqs <461147874@qq.com>
2017-07-24 18:00:38 +0800

    [misc]add eval to support expression

--- src/eval.js
+++ src/eval.js
@@ -1,3 +1,14 @@
 /**
  * Created by pomy on 24/07/2017.
  */
+
+let log = require('./log');
+
+module.exports = function evaluate (exp, data) {
+    let fn = new Function('data', 'with (data) { return ' + exp + '}');
+    try {
+        return fn(data);
+    } catch (e) {
+        log.error(`Error when evaluating filter condition: ${exp}`);
+    }
+};
